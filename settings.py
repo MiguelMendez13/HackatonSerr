c1ad10cc23205151754d45730fb178f666e0f4e2
MiguelMendez13 <62361525+MiguelMendez13@users.noreply.github.com>
2021-10-02 23:04:30 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR,"static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
